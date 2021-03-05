4640048acf63cf2df82e0126577f7b81954c558a
wcr <1161495065@qq.com>
2021-03-05 18:05:49 +0800

    add judege login in status by session

--- src/main/java/life/majiang/community/dto/AccessTokenDTO.java
+++ src/main/java/life/majiang/community/dto/AccessTokenDTO.java
@@ -2,7 +2,7 @@ package life.majiang.community.dto;
 
 public class AccessTokenDTO {
     private String client_id;
-    private String client_serect;
+    private String client_secret;
     private String code;
     private String redirect_uri;
     private String state;
@@ -16,12 +16,12 @@ public class AccessTokenDTO {
         this.client_id = client_id;
     }
 
-    public String getClient_serect() {
-        return client_serect;
+    public String getClient_secret() {
+        return client_secret;
     }
 
-    public void setClient_serect(String client_serect) {
-        this.client_serect = client_serect;
+    public void setClient_secret(String client_secret) {
+        this.client_secret = client_secret;
     }
 
     public String getCode() {
